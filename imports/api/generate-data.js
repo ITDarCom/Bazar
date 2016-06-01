49d7b41e8747e64f98b7aad72679c5ffe2b4b776
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2016-06-01 13:21:50 +0300

    style fixes

--- imports/api/generate-data.js
+++ imports/api/generate-data.js
@@ -41,7 +41,7 @@ Meteor.methods({
             const shop = opts.shop? opts.shop : Shops.findOne()
             const category = opts.category? opts.category : Categories.findOne().identifier
             const city = opts.city? opts.city : shop.city
-            const title = opts.title? opts.title: `item ${i} of ${shop.title}`
+            const title = opts.title? opts.title: `كوكيز نرويجي`
             
             var item = { 
                 _id: Math.floor((Math.random() * 10000) + 1000), 
@@ -205,9 +205,9 @@ Meteor.methods({
                 { _id: Math.floor((Math.random() * 10000) + 1000).toString(), title: `كوكيز نرويجي`, description: 'أفاق واحدة الثقيلة. دنو أم أواخر وبريطانيا. مدن ان خلاف النفط إتفاقية, لغزو عالمية لم انه. لها إذ عُقر وصغار الدولارات, دار بل السيء الربيع', shop: shopId, city: 'jeddah', createdAt: new Date(), category: 'mashawee', price: 50, thumbnails: [ {url:'/cookie.jpg'}, {url:'/cookie.jpg'}, {url:'/cookie.jpg'} ] },
             ]
 
-            /*items.forEach(function(item){
+            items.forEach(function(item){
                 Items.insert(item, { getAutoValues : false })
-            })*/
+            })
 
         })              
 
